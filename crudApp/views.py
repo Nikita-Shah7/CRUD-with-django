65e42721c53f703614b7daa1bc41b3da1d3528b4
Nikita Shah <202101209@daiict.ac.in>
2023-08-01 23:40:19 +0530

    minorUpdates

--- crudApp/views.py
+++ crudApp/views.py
@@ -6,11 +6,12 @@ def index(request):
     if request.method == 'POST':
         if 'add' in request.POST:
             # print("nikAdd")
+            checkbox = 0
             name = request.POST['name']
             email = request.POST['email']
             address = request.POST['address']
             phone = request.POST['phone']
-            newEmployee = Employee(id=id,name=name,email=email,address=address,phone=phone)
+            newEmployee = Employee(checkbox=checkbox,name=name,email=email,address=address,phone=phone)
             newEmployee.save()
         elif 'edit' in request.POST:
             # print("nikEdit")
@@ -19,7 +20,7 @@ def index(request):
             email = request.POST['email']
             address = request.POST['address']
             phone = request.POST['phone']
-            editEmployee = Employee.objects.get(id=id)
+            editEmployee = Employee.objects.filter(id=id)
             editEmployee.name = name
             editEmployee.email = email
             editEmployee.address = address
@@ -28,13 +29,19 @@ def index(request):
         elif 'delete' in request.POST:
             # print("nikDelete")
             id = request.POST['id']
-            deleteEmployee = Employee.objects.get(id=id)
+            deleteEmployee = Employee.objects.filter(id=id)
             deleteEmployee.delete()
+        elif 'deleteAll' in request.POST:
+            checkedEmployee = Employee.objects.filter(checkbox=1)
+            print(checkedEmployee)
+            for emp in checkedEmployee:
+                emp.delete()
 
     
     employee = Employee.objects.all()
+    count = Employee.objects.count()
     context = {
-        'employee' : employee
+        'employee' : employee,
+        'count': count
     }
-    print(employee)
     return render(request,"index.html",context)
